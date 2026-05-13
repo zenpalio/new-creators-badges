0d4ae7b16774a453e583653709d063f7a6194e99
sasha <best.lessons.new@gmail.com>
2026-05-13 06:54:49 +0200

    update skeleton bg

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,7 +1,7 @@
 import { cn } from "../../lib/utils";
 
 function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={cn("animate-pulse rounded-md bg-muted-v2", className)} {...props} />;
+  return <div className={cn("animate-pulse rounded-md bg-card", className)} {...props} />;
 }
 
 export { Skeleton };
